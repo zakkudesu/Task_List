a1e391924b3ba7d2faed5412fbf78ac8a306139c
Zach Alcerro <zachalcerro@gmail.com>
2019-08-26 10:38:20 +0900

    adds remove clear and filter functions to UI

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners(){
   //Add task event
   form.addEventListener('submit', addTask);
+  //Remove task event
+  taskList.addEventListener('click', removeTask);
+  //Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  //Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 //Add Task
@@ -42,4 +48,37 @@ function addTask(e){
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+//Remove Task using event delegation
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')) {
+    e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+//Clear Tasks
+function clearTasks(){
+  // taskList.innerHTML = '';
+
+  //Faster Method
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+//Filter Tasks
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
